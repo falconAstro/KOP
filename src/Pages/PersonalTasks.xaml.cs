c451e4d77545aa443aab614baf0e5096a9f6bf3b
Samuel Sokol <sjsokol198@gmail.com>
2024-12-01 16:23:25 +0100

    Upravena viditelnost personal taskov - teraz viditelne iba pre daneho uzivatela

--- src/Pages/PersonalTasks.xaml.cs
+++ src/Pages/PersonalTasks.xaml.cs
@@ -1,32 +1,42 @@
+using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Database.Query;
+using Microsoft.Maui.Storage;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace TimeManagementApp.Pages;
 
 public partial class PersonalTasks : ContentPage
 {
+    //Firebase clients
+    private readonly FirebaseAuthClient _firebaseAuthClient;
     private readonly FirebaseClient _firebaseClient;
+
+    //Zadefinovanie PersonalTask classu
     public class PersonalTask
     {
         public string Username { get; set; }
         public required string Task { get; set; }
-        public string TaskId { get; set; }
+        public string TaskID { get; set; }
     }
-    public ObservableCollection<PersonalTask> PersonalTaskList { get; set; } = [];
+    public ObservableCollection<PersonalTask> PersonalTaskList { get; set; } = []; //Zoznam Taskov nacitavanych z DB
+    public User User { get; set; } //Premenna (neskor) obsahujuca aktualne prihlaseneho usera
 
-    public PersonalTasks(FirebaseClient firebaseClient)
+    //Inicializacia stranky
+    public PersonalTasks(FirebaseClient firebaseClient, FirebaseAuthClient firebaseAuthClient)
 	{
 		InitializeComponent();
         BindingContext = this;
+        _firebaseAuthClient = firebaseAuthClient;
         _firebaseClient = firebaseClient;
     }
 
-    //Nacitanie taskov pri otvoreni stranky 
     protected override async void OnNavigatedTo(NavigatedToEventArgs args)
     {
         base.OnNavigatedTo(args);
-       
+        User = _firebaseAuthClient.User;
         await LoadTasksAsync();
     }
 
@@ -34,29 +44,38 @@ public partial class PersonalTasks : ContentPage
     public async Task LoadTasksAsync()
     {
         PersonalTaskList.Clear();
-        _firebaseClient.Child("PersonalTask").AsObservable<PersonalTask>().Subscribe((item) =>
+        _firebaseClient.Child("PersonalTask").Child(User.Uid).AsObservable<PersonalTask>().Subscribe((item) =>
         {
             if (item.Object != null)
             {
-                item.Object.TaskId = item.Key;
+                item.Object.TaskID = item.Key;
                 PersonalTaskList.Add(item.Object);
             }
         });
     }
 
-    //Vytvorenie kednoducheho tasku v databaze
+    //Vytvorenie jednoducheho tasku v databaze
     private async void BtnCreatePersonalTask_Clicked(object sender, EventArgs e)
     {
-        await _firebaseClient.Child("PersonalTask").PostAsync(new PersonalTask{Task = EntryPersonalTask.Text});
+        await _firebaseClient.Child("PersonalTask").Child(User.Uid).PostAsync(new PersonalTask{Task = EntryPersonalTask.Text});
 
         EntryPersonalTask.Text = string.Empty;
 
         await Shell.Current.DisplayAlert("", "Task created", "OK");
     }
     //Odstranenie tasku aktualne nefunkcne
-    private async void DeletePersonalTask(string Id)
+    private async void DeletePersonalTask(string TaskId)
     {
-        await _firebaseClient.Child($"PersonalTask/{Id}").DeleteAsync();
+        await _firebaseClient.Child("PersonalTask").Child(User.Uid).Child(TaskId).DeleteAsync();
         await LoadTasksAsync();
     }
+
+    //new Command DeleteTask(string TaskId,bool taskDeleted)
+    //{
+    //    _firebaseClient.Child("PersonalTask").Child(User.Uid).Child(TaskId).DeleteAsync();
+    //    LoadTasksAsync();
+        
+    //}
+    
+   
 }
