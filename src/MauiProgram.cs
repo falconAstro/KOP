4667c84fd4db81b7ebf459961649bc897e450084
Samuel Sokol <sjsokol198@gmail.com>
2025-01-02 19:08:51 +0100

    Added styles (fixed Ui issues in dark mode)

--- src/MauiProgram.cs
+++ src/MauiProgram.cs
@@ -1,8 +1,9 @@
 ﻿using Firebase.Auth;
 using Firebase.Auth.Providers;
 using Firebase.Database;
-using Microsoft.Extensions.Logging;
 using TimeManagementApp.Pages;
+using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace TimeManagementApp
 {
@@ -11,27 +12,18 @@ namespace TimeManagementApp
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkit();
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
             //Firebase client pre databazu
             builder.Services.AddSingleton(new FirebaseClient("https://timemanagement-4d83d-default-rtdb.firebaseio.com/"));
             //Firebase client pre Auth
-            builder.Services.AddSingleton(new FirebaseAuthClient(new FirebaseAuthConfig()
-            {
-                ApiKey = "AIzaSyDf5i5Ycs - ygbAFJfdPV3_kwRtTHRJra0Q",
-                AuthDomain = "timemanagement-4d83d.web.app",
-                Providers = [new EmailProvider()]
-            }));
-
+            builder.Services.AddSingleton(new FirebaseAuthClient(new FirebaseAuthConfig() { ApiKey = "AIzaSyDf5i5Ycs - ygbAFJfdPV3_kwRtTHRJra0Q", AuthDomain = "timemanagement-4d83d.web.app", Providers = [new EmailProvider()] }));
             //Pages
             builder.Services.AddSingleton<LoginPage>();
             builder.Services.AddSingleton<SignUpPage>();
@@ -40,8 +32,6 @@ namespace TimeManagementApp
             builder.Services.AddSingleton<SharedEvents>();
             builder.Services.AddSingleton<ShoppingLists>();
             builder.Services.AddSingleton<SettingsPage>();
-            
-
             return builder.Build();
         }
     }
